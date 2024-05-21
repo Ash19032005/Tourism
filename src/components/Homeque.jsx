cff048670cc29354c02b72fe3cbc3c51571ace2e
Ash19032005 <ashwinsk1905@gmail.com>
2024-05-21 17:12:09 +0530

    card and navbar change commit

--- src/components/Homeque.jsx
+++ src/components/Homeque.jsx
@@ -4,30 +4,51 @@ import Data from "../../Data/Detailed.json";
 
 const Homeque = () => {
   const data = Data.destinations;
-
   const showdetail = (index) => {
     console.log(index);
   };
-
   return (
-    <section className='grid grid-cols-3 gap-4'>
+    <section className='grid grid-cols-3 gap-4 '>
       {data.map((ele, i) => (
-        <div key={i} className='w-[20rem] h-[25rem] m-[5rem] relative flex flex-col justify-center items-center'>
-          <div className='w-[20rem] h-[30rem] overflow-hidden rounded-[30px]'>
-            <img src={data[i].image} className='rounded-[30px] w-[25rem] h-[30rem] bg-cover' alt="Heritage Image" />
-          </div>
-          <div className='w-[20rem] h-[20rem] bg-[black] mt-[-30rem] z-[2] opacity-[30%] rounded-[30px] h-[20rem] flex flex-col justify-center items-center'>
-            <h1 className='text-[4rem] mt-[0px] text-center leading-[60px] z-[2] text-[white]'>{data[i].name}</h1>
-            <p className='text-[white] z-[2] w-[18rem]'>{data[i].description}</p>
-          </div>
-            <Link to={`/detail/?id=${i}`} key={i}>
-              <button onClick={() => showdetail(i)} className='w-[80px] h-[30px] border-2 rounded-xl text-[black] backdrop-blur hover:text-[1.25rem] cursor-pointer text-[white] hover:w-[100px] hover:h-[50px] duration-[0.4s] mt-[5rem]'>Explore</button>
-            </Link>
-        </div>
 
+          <div class="group relative mt-[5rem]">
+          <div class="px-4 py-2 m-5 text-white rounded">
+          <img src={data[i].image} className='w-[25rem] h-[30rem] bg-cover' alt="Heritage Image"/>
+            </div>
+          <div class="absolute bottom-0 left-0  mb-[1.75rem] ml-[2.2rem]  w-[84.5%] h-0   bg-gradient-to-br from-green-200 via-red-200  to-transparent rounded-sm transition-height duration-500 overflow-hidden group-hover:h-[90%]">
+              <div class="flex flex-col justify-center items-center text-center text-white h-full">
+                  <h3 class="text-[2.5rem] mb-5 text-black">{data[i].name}</h3>
+                  <p className='text-lg text-gray-600'>{data[i].description}</p>
+                  <Link to={`/detail/?id=${i}`} key={i}>
+            <button onClick={() => showdetail(i)} className='w-[80px] h-[40px] mt-2 cursor-pointer  rounded-xl  hover:bg-white duration-[0.7s] text-gray-800 text-md'>Explore</button>
+            </Link>
+              </div>
+          </div>
+          </div>
       ))}
     </section>
   );
 };
 
 export default Homeque;
+
+{/* 
+// <div key={i} className='w-[20rem] h-[25rem] m-[5rem] py-10 rounded-xl relative flex flex-col justify-center items-center relative overflow-hidden '>
+        //   <div className='w-[20rem] h-[30rem] py-[-20px] overflow-hidden rounded-xl'>
+        //     <img src={data[i].image} className='w-[25rem] h-[30rem] bg-cover' alt="Heritage Image"/>
+        //   </div>
+
+        //   <div class="absolute bottom-0 left-0 w-[10rem] h-0 bg-gradient-to-t from-black to-ff004f bg-opacity-60 rounded-md transition-height duration-500 overflow-hidden group-hover:h-full">
+        //     <div class="flex flex-col justify-center items-center text-center text-white h-full">
+        //         <h3 class="font-medium mb-5">Heading</h3>
+        //         <button>Explore</button>
+        //     </div>
+        //   </div> */}
+
+          {/* <div className=' mt-[-30rem] z-[2] rounded-[30px] h-[20rem] flex flex-col justify-center items-center'>
+            <h1 className='text-[2rem] mt-[0px] text-center leading-[60px] z-[2] text-[white]'>{data[i].name}</h1>
+            <Link to={`/detail/?id=${i}`} key={i}>
+            <button onClick={() => showdetail(i)} className='w-[80px] h-[30px]  rounded-xl text-[black] backdrop-blur  cursor-pointer text-[white] duration-[0.4s] mt-[5rem]'>Explore</button>
+            </Link>
+            </div> */}
+        {/* // </div> */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
 function Navbar() {
   return (
-    <section className='flex justify-evenly items-evenly shadow-xl fixed top-0 w-full bg-white z-10 '>
+    <section className='flex justify-between shadow-xl fixed top-0 w-full bg-white z-10 '>
       <style>
   @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Montserrat:wght@500&display=swap');
 </style>
-        <div className='align-left my-auto flex'>
+        <div className='ml-[20px]'>
             <h1 className='text-[3rem] ml-[25px] font-[Agbalumo]'>Bharat <span id='samarkand'>Yatra</span></h1>
         </div>
-        <div className='flex my-auto'>
-            <Link to='/'><li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Home</a></li></Link>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">Places</a></li>
-            <li className='list-none text-xl hover:text-gray-500 m-[10px]'><a href="">About Us</a></li>
+        <div className='flex mt-5 mr-5 gap-5'>
+           <Link><li className='list-none text-xl text-black-400 transition ease-in-out delay-50 bg-white  hover:scale-110 hover:bg-gray-400  duration-200   p-2 rounded-xl'><a href="">Home</a></li></Link>
+           <Link><li className='list-none text-xl hover:bg-gray-400  transition ease-in-out delay-50 bg-white  hover:scale-110 hover:bg-gray-400  duration-200  p-2 rounded-xl'><a href="">Places</a></li></Link>
+            <Link><li className='list-none text-xl hover:bg-gray-400  transition ease-in-out delay-50 bg-white  hover:scale-110 hover:bg-gray-400  duration-200  p-2 rounded-xl'><a href="">About Us</a></li></Link>
         </div>
     </section>
   )
